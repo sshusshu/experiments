8636b90ebb0145cbf861932277415085eb18e1b6
subin <subin9033@gmail.com>
2021-03-23 09:52:00 +0900

    setup5

--- index.js
+++ index.js
@@ -43,36 +43,39 @@ const getRGBA = e => {
 
 
     let  manipulatedImgData = [] ;
-    for (let i = 0; i < canvasArr.length - 1; i++) {
-        for (let j = 0; j < img.width - 3; j++) {
-            let r = Math.floor((canvasArr[i][j][0] + canvasArr[i][j + 1][0] + canvasArr[i][j + 2][0] + canvasArr[i][j + 3][0] +
-                                   canvasArr[i + 1][j][0] + canvasArr[i + 1][j + 1][0] + canvasArr[i + 1][j + 2][0]+ canvasArr[i + 1][j + 3][0]) / 8)
-            let g = Math.floor((canvasArr[i][j][1] + canvasArr[i][j + 1][1] + canvasArr[i][j + 2][1] + canvasArr[i][j + 3][1]+
-                                   canvasArr[i + 1][j][1] + canvasArr[i + 1][j + 1][1] + canvasArr[i + 1][j + 2][1]+ canvasArr[i + 1][j + 3][1]) / 8)
-            let b = Math.floor((canvasArr[i][j][2] + canvasArr[i][j + 1][2] + canvasArr[i][j + 2][2] +  canvasArr[i][j + 3][2] +
-                                   canvasArr[i + 1][j][2] + canvasArr[i + 1][j + 1][2] + canvasArr[i + 1][j + 2][2]+ canvasArr[i + 1][j + 3][2]) / 8)
+    for (let i = 0; i < canvasArr.length - 1; i+=2) {
+        for (let j = 0; j < img.width - 6; j+=7) {
+            let r = Math.floor((canvasArr[i][j][0] + canvasArr[i][j + 1][0] + canvasArr[i][j + 2][0] + canvasArr[i][j + 3][0] + canvasArr[i][j + 4][0] + canvasArr[i][j + 5][0] + canvasArr[i][j + 6][0] +
+                canvasArr[i + 1][j][0] + canvasArr[i + 1][j + 1][0] + canvasArr[i + 1][j + 2][0]+ canvasArr[i + 1][j + 3][0] + canvasArr[i + 1][j + 4][0] + canvasArr[i + 1][j + 5][0]+ canvasArr[i + 1][j + 6][0]) / 12)
+            let g = Math.floor((canvasArr[i][j][1] + canvasArr[i][j + 1][1] + canvasArr[i][j + 2][1] + canvasArr[i][j + 3][1] + canvasArr[i][j + 4][1] + canvasArr[i][j + 5][1] + canvasArr[i][j + 6][1] +
+                canvasArr[i + 1][j][1] + canvasArr[i + 1][j + 1][1] + canvasArr[i + 1][j + 2][1]+ canvasArr[i + 1][j + 3][1] + canvasArr[i + 1][j + 4][1] + canvasArr[i + 1][j + 5][1]+ canvasArr[i + 1][j + 6][1]) / 12)
+            let b = Math.floor((canvasArr[i][j][2] + canvasArr[i][j + 1][2] + canvasArr[i][j + 2][2] + canvasArr[i][j + 3][2] + canvasArr[i][j + 4][2] + canvasArr[i][j + 5][2] + canvasArr[i][j + 6][2] +
+                canvasArr[i + 1][j][2] + canvasArr[i + 1][j + 1][2] + canvasArr[i + 1][j + 2][2]+ canvasArr[i + 1][j + 3][2] + canvasArr[i + 1][j + 4][2] + canvasArr[i + 1][j + 5][2]+ canvasArr[i + 1][j + 6][2]) / 12)
 
             const div = document.createElement('div');
             body.appendChild(div)
             div.classList.add('.div')
             div.style.backgroundColor = `rgba(${r},${g},${b},255)`
             manipulatedImgData.push(r,g,b,255)
-
         }
         const br = document.createElement('br');
         body.appendChild(br)
     }
 
-let myImgData = ctx2.createImageData(canvas.width,canvas.height);
-
-    for (let i =0 ;myImgData.data.length;i+=4){
-        myImgData.data[i+0] = manipulatedImgData[i+0];
-        myImgData.data[i+1] = manipulatedImgData[i+1];
-        myImgData.data[i+2] = manipulatedImgData[i+2];
-        myImgData.data[i+3] = manipulatedImgData[i+3];
+let myImgData = ctx2.createImageData(img.width,canvasArr.length/2);
+    for (let i =0 ;i<myImgData.data.length;i+=4){
+        for(let j =0;j<img.width-6; j+=7){
+            myImgData.data[j+0] = manipulatedImgData[i+0];
+            myImgData.data[j+1] = manipulatedImgData[i+1];
+            myImgData.data[j+2] = manipulatedImgData[i+2];
+            myImgData.data[j+3] = manipulatedImgData[i+3];
+        }
     }
+    
+console.log(myImgData)
 console.log(manipulatedImgData)
-   ctx2.putImageData(manipulatedImgData,0,0,)
+
+   ctx2.putImageData(myImgData,0,0)
 
     // const path = new Path2D('M46,46s93-17,149-6,7,25,7,25L123,77,56,71S-26,72,46,46Z');
     // ctx.fillStyle = 'black'
