393cb8e712a41a04a24f325e10fa7bc50a6e0391
subin <subin9033@gmail.com>
2021-05-10 06:52:00 +0900

    Fix : javascript file

--- parallax/airpadpro/app.js
+++ parallax/airpadpro/app.js
@@ -1,7 +1,7 @@
 const intro = document.querySelector(".intro");
 const video = intro.querySelector("video");
 const text = intro.querySelector("h1");
-
+const ex = undefined;
 //END SECTION
 const section = document.querySelector("section");
 const end = section.querySelector("h1");
