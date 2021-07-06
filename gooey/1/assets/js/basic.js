6181d6d62482425346c8e4d8dfec87ec9114ec95
subin <subin9033@gmail.com>
2021-07-07 06:52:00 +0900

    3Create: copy html

--- gooey/1/assets/js/basic.js
+++ gooey/1/assets/js/basic.js
@@ -2,7 +2,7 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
-
+const ex = undefined;
 let mouse = {
   x: 0,
   y: 0,
